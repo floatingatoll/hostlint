690b50f072b4558da28efa81a9a58111288d9138
Wesley Dawson <wdawson@mozilla.com>
2011-08-12 02:31:22 -0700

    eth{0,1} bonded check

---
+++ client/examples/ethernet_bonded.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# "verify that eth0 and eth1 are bonded"
+# this should likely be generalized
+
+if [[ -d /proc/net/bonding ]]  ; then
+  for file in /proc/net/bonding/* ; do
+    if grep 'eth0' $file && grep 'eth1' $file &>/dev/null ; then
+      echo "network.ethernet.bonded: [OK]"
+    fi
+  done
+fi
+
+# fell thru
+echo "network.ethernet.bonded: [FAIL]
+output of ifconfig:
+$(ifconfig 2>/dev/null)"
