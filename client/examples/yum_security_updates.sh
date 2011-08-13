1dd79f1de3712b247e82eb31738deb41cc933613
Wesley Dawson <wdawson@mozilla.com>
2011-08-13 00:18:27 -0700

    Suggest a command for yum security updates

--- client/examples/yum_security_updates.sh
+++ client/examples/yum_security_updates.sh
@@ -5,6 +5,7 @@ output="$(yum --security check-update 2>/dev/null)"
 
 if [[ $? == 100 ]] ; then #security updates exist!
     echo "yum.security.updates.available: [FAIL]"
+    echo "you could try \"yum --security update\""
     echo "$output" | tail -n +3
 else
     echo "yum.security.updates.available: [OK]"
